c4a56847756aa58ce38f336f48d5d561b64ca1e3
shawntsai0312 <shawntsai0312@gmail.com>
2023-11-30 14:33:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# IC Design 23Fall HW4
+##### author : B10901176 蔡弘祥
+
+#### Before Running
+```shell
+source ./tool.sh
+```
+
+#### How To Run
+```shell
+cd src
+./check.sh
+./delay.sh
+```
+
+#### Cases
+|number  |cases          |
+|--------|---------------|
+|0       |high card      |
+|1       |one pair       |
+|2       |two pair       |
+|3       |three of a kind|
+|4       |straight       |
+|5       |flush          |
+|6       |full house     |
+|7       |four of a kind |
+|8       |straight flush |
+
+#### Info
+* For more information, please check out pdfs in the doc directory
---
+++ src/sigmoid.v
@@ -0,0 +1,47 @@
+module sigmoid (
+	input         clk,
+	input         rst_n,
+	input         i_in_valid,
+	input  [ 7:0] i_x,
+	output [15:0] o_y,
+	output        o_out_valid,
+	output [50:0] number
+);
+
+// Your design
+
+endmodule
+
+//BW-bit FD2
+module REGP#(
+	parameter BW = 2
+)(
+	input           clk,
+	input           rst_n,
+	output [BW-1:0] Q,
+	input  [BW-1:0] D,
+	output [  50:0] number
+);
+
+wire [50:0] numbers [0:BW-1];
+
+genvar i;
+generate
+	for (i=0; i<BW; i=i+1) begin
+		FD2 f0(Q[i], D[i], clk, rst_n, numbers[i]);
+	end
+endgenerate
+
+//sum number of transistors
+reg [50:0] sum;
+integer j;
+always @(*) begin
+	sum = 0;
+	for (j=0; j<BW; j=j+1) begin 
+		sum = sum + numbers[j];
+	end
+end
+
+assign number = sum;
+
+endmodule
